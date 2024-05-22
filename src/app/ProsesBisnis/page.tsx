0cc1bf80317ed6db8a983d42a3ec449b444a36bb
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-05-22 12:16:25 +0700

    contoh isian real spbe proses bisnis

--- src/app/ProsesBisnis/page.tsx
+++ src/app/ProsesBisnis/page.tsx
@@ -1,20 +1,23 @@
-import Table from "@/components/global/Table/Table"
-import Link from "next/link"
+import Table from "@/components/global/Table/Table";
+import Link from "next/link";
 
 const ProsesBisnis = () => {
+  return (
+    <>
+      <div className="mb-5 flex justify-between items-center">
+        <h1 className="uppercase font-bold ">
+          Proses Bisnis Badan Perencanaan, Penelitian dan Pengembangan Daerah
+          Tahun 2024
+        </h1>
+        <Link href="/ProsesBisnis/TambahData">
+          <button className="bg-emerald-300 p-3 text-white rounded-lg hover:bg-emerald-500">
+            tambah data
+          </button>
+        </Link>
+      </div>
+      <Table />
+    </>
+  );
+};
 
-
-    return(
-        <>
-            <div className="mb-5 flex justify-between items-center">
-                <h1 className="uppercase font-bold ">Proses Bisnis Badan Penelitian BAPPEDA Tahun 2024</h1>
-                <Link href="/ProsesBisnis/TambahData">
-                    <button className="bg-emerald-300 p-3 text-white rounded-lg hover:bg-emerald-500">tambah data</button>
-                </Link>
-            </div>
-            <Table />  
-        </>
-    )
-}
-
-export default ProsesBisnis
+export default ProsesBisnis;
--- src/components/global/Table/Table.tsx
+++ src/components/global/Table/Table.tsx
@@ -1,274 +1,55 @@
-'use client'
+"use client";
 
-function Table () {
-    return(
-        <>
-            <div className="overflow-auto">
-                <table className="w-full text-sm text-left">
-                    <thead className="text-xs text-gray-700 uppercase border">
-                        <tr>
-                            <th className="px-6 py-3">
-                                Proses Bisnis
-                            </th>
-                            <th className="px-6 py-3">
-                                Sasaran Kota
-                            </th>
-                            <th className="px-6 py-3">
-                                Kode
-                            </th>
-                            <th className="px-6 py-3">
-                                Bidang Urusan
-                            </th>
-                            <th className="px-6 py-3">
-                                RAD Level 1
-                            </th>
-                            <th className="px-6 py-3">
-                                RAD Level 2
-                            </th>
-                            <th className="px-6 py-3">
-                                RAD Level 3
-                            </th>
-                            <th className="px-6 py-3">
-                                RAD Level 4
-                            </th>
-                            <th className="px-6 py-3">
-                                RAD Level 5
-                            </th>
-                            <th className="px-6 py-3">
-                                RAD Level 6
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="border rounded-b-lg">
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Kota Madiun
-                            </td>
-                            <td className="px-6 py-4">
-                                1293129
-                            </td>
-                            <td className="px-6 py-4">
-                                5 - UNSUR PENUNJANG URUSAN PEMERINTAHAN
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                        </tr>
-                        <tr className="border rounded-b-lg">
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Kota Madiun
-                            </td>
-                            <td className="px-6 py-4">
-                                1293129
-                            </td>
-                            <td className="px-6 py-4">
-                                5 - UNSUR PENUNJANG URUSAN PEMERINTAHAN
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                        </tr>
-                        <tr className="border rounded-b-lg">
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Kota Madiun
-                            </td>
-                            <td className="px-6 py-4">
-                                1293129
-                            </td>
-                            <td className="px-6 py-4">
-                                5 - UNSUR PENUNJANG URUSAN PEMERINTAHAN
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                        </tr>
-                        <tr className="border rounded-b-lg">
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Kota Madiun
-                            </td>
-                            <td className="px-6 py-4">
-                                1293129
-                            </td>
-                            <td className="px-6 py-4">
-                                5 - UNSUR PENUNJANG URUSAN PEMERINTAHAN
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                        </tr>
-                        <tr className="border rounded-b-lg">
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Kota Madiun
-                            </td>
-                            <td className="px-6 py-4">
-                                1293129
-                            </td>
-                            <td className="px-6 py-4">
-                                5 - UNSUR PENUNJANG URUSAN PEMERINTAHAN
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                        </tr>
-                        <tr className="border rounded-b-lg">
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Kota Madiun
-                            </td>
-                            <td className="px-6 py-4">
-                                1293129
-                            </td>
-                            <td className="px-6 py-4">
-                                5 - UNSUR PENUNJANG URUSAN PEMERINTAHAN
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                        </tr>
-                        <tr className="border rounded-b-lg">
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Kota Madiun
-                            </td>
-                            <td className="px-6 py-4">
-                                1293129
-                            </td>
-                            <td className="px-6 py-4">
-                                5 - UNSUR PENUNJANG URUSAN PEMERINTAHAN
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                            <td className="px-6 py-4">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quas
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
+function Table() {
+  return (
+    <>
+      <div className="overflow-auto">
+        <table className="w-full text-sm text-left">
+          <thead className="text-xs text-gray-700 uppercase border">
+            <tr>
+              <th className="px-6 py-3">No.</th>
+              <th className="px-6 py-3">Proses Bisnis</th>
+              <th className="px-6 py-3">Sasaran Kota</th>
+              <th className="px-6 py-3">Bidang Urusan</th>
+              <th className="px-6 py-3">RAD Level 1</th>
+              <th className="px-6 py-3">RAD Level 2</th>
+              <th className="px-6 py-3">RAD Level 3</th>
+              <th className="px-6 py-3">RAD Level 4</th>
+              <th className="px-6 py-3">RAD Level 5</th>
+              <th className="px-6 py-3">RAD Level 6</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border rounded-b-lg">
+              <td className="px-6 py-4">1</td>
+              <td className="px-6 py-4">Penyusunan Pohon Kinerja</td>
+              <td className="px-6 py-4">Meningkatnya Akuntabilitas Kinerja</td>
+              <td className="px-6 py-4">
+                5 - UNSUR PENUNJANG URUSAN PEMERINTAHAN
+              </td>
+              <td className="px-6 py-4">RAB.09 PEMERINTAHAN UMUM</td>
+              <td className="px-6 py-4">
+                RAB.09.05 PERENCANAAN PEMBANGUNAN NASIONAL
+              </td>
+              <td className="px-6 py-4">
+                RAB.09.05.06 PENGAWASAN, PEMANTAUAN, DAN PENGENDALIAN
+                PEMBANGUNAN NASIONAL
+              </td>
+              <td className="px-6 py-4">
+                Meningkatnya Kualitas Perencanaan Tematik Pembangunan
+              </td>
+              <td className="px-6 py-4">
+                Peningkatan Kualitas Pohon kinerja perangkat daerah
+              </td>
+              <td className="px-6 py-4">
+                penyusunan pohon kinerja Kota Madiun
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
-export default Table
+export default Table;
